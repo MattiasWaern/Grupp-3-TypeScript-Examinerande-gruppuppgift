7f1ac740e7f02904b190bea33e862734d12c06a6
ReggeMeister <80971552+MattiasWaern@users.noreply.github.com>
2026-09-01 21:42:16 +0200

    Klar med html

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,57 @@ export default function BookingPage(){
           </div>
         </div>
 
-        
+        <div className="booking-details">
+          <div>
+            <span>Startdatum</span>
+            <strong>10 september 2026</strong>
+          </div>
+
+          <div>
+            <span>Slutdatum</span>
+            <strong>15 september 2026</strong>
+          </div>
+
+          <div>
+            <span>Antal dagar</span>
+            <strong>5</strong>
+          </div>
+
+          <div>
+            <span>Total pris</span>
+            <strong>3 500 kr</strong>
+          </div>
+        </div>
+
+        <div className="booking-form-container">
+          <label htmlFor="email">
+            Din e-postadress
+          </label>
+
+          <input
+          id="email"
+          type="email"
+          placeholder="namn@exempel.se"
+          value={email}
+          onChange={((event) => setEmail(event.target.value))}
+          />
+
+          <p className="email-description">
+            Vi skickar bokningsbekräftelse till deaa e-post
+          </p>
+
+          <div className="information-box">
+            <strong>Viktig information</strong>
+
+            <p>
+              Fri avbokning fram till 24h innan bokning
+            </p>
+          </div>
+
+          <button className="confirm-button">
+            Bekräfta bokning
+          </button>
+        </div>
       </section>
     </main>
   );
