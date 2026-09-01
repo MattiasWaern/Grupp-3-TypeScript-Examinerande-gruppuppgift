d8d9531c2f1b038cfd49406f4e382337c23e2453
ReggeMeister <80971552+MattiasWaern@users.noreply.github.com>
2026-09-01 21:31:42 +0200

    booking summary

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,31 @@ export default function BookingPage(){
   const [email, setEmail] = useState("");
 
   return (
+    <main className="booking-page">
+      <button className="back-button">
+        Tillbaka till resultat
+      </button>
 
+      <h1>Boka Volvo XC40</h1>
+
+      <section className="booking-content">
+        <div className="booking-summary">
+          <div className="car-summary">
+            <img
+              src="https://images.unsplash.com/photo-1563720223185-11003d516935"
+              alt="Volvo XC40"
+              className="car-image"
+              />
+
+              <div className="car-info">
+                <h2>Volvo XC40</h2>
+                <p>700kr / dag</p>
+              </div>
+          </div>
+        </div>
+
+        
+      </section>
+    </main>
   );
 }
