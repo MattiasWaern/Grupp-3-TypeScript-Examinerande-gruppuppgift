dd3606ac21824b72219b7ae9b09e45419a87428a
Fluuffyyy <lumpymango95@gmail.com>
2026-09-02 11:12:38 +0200

    Test på carCard fungerar. Helena fixar interface så jag kan fixa resten sen.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import CarCard from "../src/Components/CarCard";
 
+const testCar = {
+  id: "1",
+  brand: "Volvo",
+  model: "XC40",
+  year: 2024,
+  pricePerDay: 699,
+  available: true,
+};
 
+function App() {
+  return (
+    <>
+      <h1>Välkommen till våran fina app :D</h1>
 
-function App(){
-  return(
-  <h1>Välkommen till våran fina app :D</h1>
-)
+      <CarCard car={testCar} />
+    </>
+  );
 }
 
-
-export default App
+export default App;
--- src/Components/CarCard.tsx
+++ src/Components/CarCard.tsx
@@ -1,2 +1,28 @@
 import type { Car } from "../types/bookingTypes";
 
+type CarCardProps = {
+  car: Car;
+};
+
+function CarCard({ car }: CarCardProps) {
+  return (
+    <article className="car-card">
+      <div className="car-card__content">
+        <h2>
+          {car.brand} {car.model}
+        </h2>
+
+        <p>Årsmodell: {car.year}</p>
+        <p>{car.pricePerDay} kr/dag</p>
+
+        <p>
+          {car.available ? "Tillgänglig" : "Inte tillgänglig"}
+        </p>
+
+        <button>Visa detaljer</button>
+      </div>
+    </article>
+  );
+}
+
+export default CarCard;
