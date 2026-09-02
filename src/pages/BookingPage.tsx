a7e794be5070647ad7a421e8cd9c62bb170a6e2a
ReggeMeister <80971552+MattiasWaern@users.noreply.github.com>
2026-09-02 14:58:14 +0200

    rättade fel i html + ändrade knapp färg

--- src/pages/BookingPage.tsx
+++ src/pages/BookingPage.tsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
 import "../style/Booking.css";
 
-
-export default function BookingPage(){
+export default function BookingPage() {
   const [email, setEmail] = useState("");
 
   return (
@@ -20,34 +19,34 @@ export default function BookingPage(){
               src="https://images.unsplash.com/photo-1563720223185-11003d516935"
               alt="Volvo XC40"
               className="car-image"
-              />
+            />
 
-              <div className="car-info">
-                <h2>Volvo XC40</h2>
-                <p>700kr / dag</p>
-              </div>
-          </div>
-        </div>
-
-        <div className="booking-details">
-          <div>
-            <span>Startdatum</span>
-            <strong>10 september 2026</strong>
+            <div className="car-info">
+              <h2>Volvo XC40</h2>
+              <p>700 kr / dag</p>
+            </div>
           </div>
 
-          <div>
-            <span>Slutdatum</span>
-            <strong>15 september 2026</strong>
-          </div>
+          <div className="booking-details">
+            <div>
+              <span>Startdatum</span>
+              <strong>10 september 2026</strong>
+            </div>
 
-          <div>
-            <span>Antal dagar</span>
-            <strong>5</strong>
-          </div>
+            <div>
+              <span>Slutdatum</span>
+              <strong>15 september 2026</strong>
+            </div>
 
-          <div>
-            <span>Total pris</span>
-            <strong>3 500 kr</strong>
+            <div>
+              <span>Antal dagar</span>
+              <strong>5</strong>
+            </div>
+
+            <div>
+              <span>Totalt pris</span>
+              <strong>3 500 kr</strong>
+            </div>
           </div>
         </div>
 
@@ -57,22 +56,22 @@ export default function BookingPage(){
           </label>
 
           <input
-          id="email"
-          type="email"
-          placeholder="namn@exempel.se"
-          value={email}
-          onChange={((event) => setEmail(event.target.value))}
+            id="email"
+            type="email"
+            placeholder="namn@exempel.se"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
           />
 
           <p className="email-description">
-            Vi skickar bokningsbekräftelse till deaa e-post
+            Vi skickar bokningsbekräftelsen till denna e-post
           </p>
 
           <div className="information-box">
             <strong>Viktig information</strong>
 
             <p>
-              Fri avbokning fram till 24h innan bokning
+              Fri avbokning fram till 24 timmar innan bokningsstart.
             </p>
           </div>
 
