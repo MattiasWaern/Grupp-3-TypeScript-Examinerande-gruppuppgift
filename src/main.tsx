97b9a8f1b1efda55275954ef2fee3325b00ea57a
ReggeMeister <80971552+MattiasWaern@users.noreply.github.com>
2026-09-02 15:01:36 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import BookingPage from './pages/BookingPage'
+//import BookingPage from './pages/BookingPage'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BookingPage />
+
   </StrictMode>,
 )
