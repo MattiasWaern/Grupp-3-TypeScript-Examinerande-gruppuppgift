ddf6a050a83f710a0935389126bab3289e2df990
SofiaArf <sofia.arfwedson@hotmail.com>
2026-09-06 22:27:30 +0200

    La till en navbar med ikoner, tog bort test från App.jsx

---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,27 @@
+import "../style/Navbar.css";
+
+
+import { FaCar, FaEnvelope, FaUser } from "react-icons/fa";
+
+export default function Navbar() {
+  return (
+
+    <nav className="navbar">
+
+      <span className="navbar-logo">
+        <FaCar /> Hyrbil
+      </span>
+
+
+      <ul className="navbar-links">
+        <li>Hem</li>
+        <li>Mina bokningar</li>
+        <li>Om oss</li>
+      </ul>
+      <span className="navbar-icons">
+        <FaEnvelope />
+        <FaUser />
+      </span>
+    </nav>
+  );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,16 @@
 import "../style/Home.css";
+import Navbar from "../Components/Navbar";
 
 export default function Home() {
   return (
- 
+  <>
+    <Navbar />
     <section className="hero">
-   
       <article className="hero-left">
         <h1>Hyr bilen som passar dig</h1>
         <p>Välj datum och hitta tillgängliga bilar på några sekunder!</p>
-
-    
       </article>
     </section>
-  );
+  </>
+);
 }
