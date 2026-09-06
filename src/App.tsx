86644823a2706bb69ae395d7916bd071e7fb7cd5
SofiaArf <sofia.arfwedson@hotmail.com>
2026-09-06 22:29:14 +0200

    Refactor Home component layout and add styling for hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,3 @@
-// import CarCard from "../src/Components/CarCard";
-
-// const testCar = {
-//   id: "1",
-//   brand: "Volvo",
-//   model: "XC40",
-//   year: 2024,
-//   pricePerDay: 699,
-//   available: true,
-// };
-
-// function App() {
-//   return (
-//     <>
-//       <h1>Välkommen till våran fina app :D</h1>
-
-//       <CarCard car={testCar} />
-//     </>
-//   );
-// }
-
-// export default App;
 import Home from "./pages/Home";
 
 function App() {
