713c7e3430b823071489b4f02839447f9b3b1524
ReggeMeister <80971552+MattiasWaern@users.noreply.github.com>
2026-09-07 13:21:30 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
 import BookingPage from './pages/BookingPage'
-import BookingConfirmation from './pages/BookingConfirmation.tsx'
+// import BookingConfirmation from './pages/BookingConfirmation.tsx'
 
 function App() {
   return <BookingPage />;
