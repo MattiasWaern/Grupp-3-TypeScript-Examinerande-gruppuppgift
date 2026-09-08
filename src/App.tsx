a7887a24b3ba7f6deb945c4db33d45f7818aaba2
Fluuffyyy <lumpymango95@gmail.com>
2026-09-08 16:18:26 +0200

    La tillbaka app.tsx till orginal, ändrade då jag behövde för test tills routing är klart

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import CarDetailPage from "./pages/CarDetailPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import BookingPage from "./pages/BookingPage";
+import BookingConfirmation from "./pages/BookingConfirmation";
+import About from "./pages/About";
 
-function App() {
-  return <CarDetailPage />;
+
+
+export default function App() {
+return (
+<BrowserRouter>
+<Routes>
+<Route path="/" element={<Home />} />
+<Route path="/booking" element={<BookingPage />} />
+<Route path="/booking-confirmation" element={<BookingConfirmation />} />
+<Route path="/about" element={<About />} />
+</Routes>
+</BrowserRouter>
+);
 }
-
-export default App;
