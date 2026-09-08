2ccc21e55c26e3933cefa56f05d84cec864572f1
ReggeMeister <80971552+MattiasWaern@users.noreply.github.com>
2026-09-08 12:10:44 +0200

    commit
    gör så sofias o mina routes funkar tillsammans

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,20 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import BookingPage from "./pages/BookingPage";
 import BookingConfirmation from "./pages/BookingConfirmation";
-import Home from "./pages/Home";
+import About from "./pages/About";
 
-function App() {
+
+
+export default function App() {
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={<Home />}
-      />
-
-      <Route
-        path="/booking"
-        element={<BookingPage />}
-      />
-
-      <Route
-        path="/booking-confirmation"
-        element={<BookingConfirmation />}
-      />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/booking" element={<BookingPage />} />
+        <Route path="/booking-confirmation" element={<BookingConfirmation />} />        
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
